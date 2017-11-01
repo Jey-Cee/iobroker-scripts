9c34068d9f123edc0c0549829821bf0ffa17f1e2
Jey-Cee <christoph@von-gerle.de>
2017-11-01 13:06:09 +0100

    Update getChefkoch.js

--- Essensplan/getChefkoch.js
+++ Essensplan/getChefkoch.js
@@ -1,11 +1,12 @@
 //Author: Jey Cee;      e-mail: jey-cee@live.com 
 //
 //Script Name: Chefkoch_Rezept_holen
-//Version: 0.1.1 Erstellungsdatum: 13.05.2016
+//Version: 0.1.2 Erstellungsdatum: 13.05.2016
 //Beschreibung: Liest ein Rezept anhand des Links von der Seite Chefkoch.de und überträgt die Daten als reinen String in zuvor angelegte Objekte.
 //              Wichtig: Die Objekt struktur wird vom script "Essensplan" erstellt.
 //
 //Changelog:
+//          01.11.2017 0.1.2: Umlautformattierung im Namen angepasst
 //          16.04.2017 0.1.1: Zubereitung wurde durch Änderungen bei Chefkoch nicht mehr abgerufen. Fixed.
 
 var link;
@@ -26,6 +27,7 @@ function findeZubereitung (body) {
     text1 = text1.replace(/\t/g, "");
     text1 = text1.replace(/\n/g, "");
     text1 = text1.replace(/\s\s*/g, " ");
+    try{text1 = text1.replace(/\"/g, "");} catch(err){}
     
     setState("javascript.0.Essensplan." + Tag + ".Zubereitung"/*Essensplan." + Tag + ".Zubereitung*/, text1);
 
@@ -67,12 +69,18 @@ function findeRezeptName (body) {
     text1 = text1.exec(body);
     text1 = text1.toString();
     text1 = text1.replace('<h1 class="page-title">', "");
-    text1 = text1.replace('</h1>', "");
-    text1 = text1.replace(/ä/g, "ae");
-    text1 = text1.replace(/ö/g, "oe");
-    text1 = text1.replace(/ü/g, "ue");
-    text1 = text1.replace(/&auml;/g, "ae");
+    try{text1 = text1.replace('</h1>', "");}catch(err){}
+    try{text1 = text1.replace(/&Auml;/g, "Ä");}catch(err){}
+    try{text1 = text1.replace(/&auml;/g, "ä");}catch(err){}
+    try{text1 = text1.replace(/&Ouml;/g, "Ö");}catch(err){}
+    try{text1 = text1.replace(/&ouml;/g, "ö");}catch(err){}
+    try{text1 = text1.replace(/&Uuml;/g, "Ü");}catch(err){}
+    try{text1 = text1.replace(/&uuml;/g, "ü");}catch(err){}
+    try{text1 = text1.replace(/&szlig;/g, "ß");}catch(err){}
+    try{text1 = text1.replace(/\'/g, "");}catch(err){}
+
     setState("javascript.0.Essensplan." + Tag + ".Name"/*Essensplan." + Tag + ".Name*/, text1);
+    
 }
 
 function leseWebseite () {
